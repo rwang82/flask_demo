cc806da9dd3ecb0c72a1b1ad4509ea6e644a4e41
王宇锋 <wangyufeng1@cmcm.com>
2019-04-18 15:08:03 +0800

    支持接口登录接口.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session, redirect, url_for, escape, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,13 @@ def hello_world():
 @app.route('/chilema/')
 def chilema():
     return 'hai meiyou chi ne, dao ni jia chi qu.'
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return "recevie POST login request."
+    return "recevie GET login request."
 
 #if __name__ == '__main__':
 #    app.run()
