d87b82df0685cb1e98301a28281c294e97efc946
王宇锋 <wangyufeng1@cmcm.com>
2019-04-18 17:02:45 +0800

    解析 login 中请求code并打印出来.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, session, redirect, url_for, escape, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print ('abcdef')
     return 'Hello World! (THIS IS ROM HERE!!!)'
 
 @app.route('/chilema/')
@@ -14,6 +16,9 @@ def login():
     if request.method == 'POST':
         session['username'] = request.form['username']
         return "recevie POST login request."
+    code = request.args.get("code","")
+    #app.logger.info('recevie login code:' + code)
+    print ("[ROM_DEBUG], code:" + code)
     return "recevie GET login request."
 
 #if __name__ == '__main__':
