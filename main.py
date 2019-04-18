2a36d200e15402818d1657a4c7a9c6acd3094000
rom.wang <rwang82@gmail.com>
2019-04-18 17:51:30 +0800

    服务端请求到了session_key.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, session, redirect, url_for, escape, request
 
 app = Flask(__name__)
@@ -19,7 +20,15 @@ def login():
     code = request.args.get("code","")
     #app.logger.info('recevie login code:' + code)
     print ("[ROM_DEBUG], code:" + code)
+    authCode2Session(code)
     return "recevie GET login request."
+
+def authCode2Session(code):
+    APPID = "wx0feab39f50fd2674"
+    SECRET = "39645c2ec117df4c5b8f8f91b7598623"
+    reqUrl = "https://api.weixin.qq.com/sns/jscode2session?appid="+APPID+"&secret="+SECRET+"&js_code="+code+"&grant_type=authorization_code"
+    r = requests.get(reqUrl)
+    print("[ROM_DEBUG], r:"+r.text)
 
 #if __name__ == '__main__':
 #    app.run()
